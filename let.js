f417b2b7940165b0c541baf1fb151120867ab4a6
CaptStorm <cptstormbird@gmail.com>
2017-10-15 20:42:37 -0700

    Update let.js

--- let.js
+++ let.js
@@ -7,3 +7,27 @@ if(true) {
   console.log(age);
    };
 console.log(age);
+
+
+
+'use strict';
+
+let age;
+age = 90;
+
+console.log("your age is " + age);
+
+function doSomething(){
+  age = 39;
+  console.log("age within the function doSomething is " + age);
+};
+
+doSomething();
+
+
+
+
+
+
+
+

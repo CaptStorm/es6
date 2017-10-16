058324fea8ed2b57976b7ef854bd761397db6087
CaptStorm <cptstormbird@gmail.com>
2017-10-16 10:51:31 -0700

    Create templateLiteral.js

---
+++ templateLiteral.js
@@ -0,0 +1,7 @@
+//the whole idea behind template literal is to shorten the concat and make it simple to use
+//use back ticks ...back ticks are located right next to number 1 and above the tab 
+
+
+let a = `good`;
+let b = `${a} morning`;
+console.log(b);

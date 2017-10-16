245e1f025f9ad31eceb24cbe508bae41c052f6fc
CaptStorm <cptstormbird@gmail.com>
2017-10-15 21:16:13 -0700

    Update array.js

--- array.js
+++ array.js
@@ -6,3 +6,16 @@ console.log(age);
 
 [23, 44, 55, 77, 90, 39]
 [23, 39, 44, 55, 77, 89, 90]
+
+
+
+let number = [89, 22, 21, 664, 992];
+
+function sumUp(toAdd) {
+    let result = 0;
+    for (let i = 0; i < toAdd.length; i++) {
+        result += toAdd[i];
+    }
+    return result;
+};
+console.log(sumUp(number));

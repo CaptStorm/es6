df858ef3fae40dc69b4a537940120001625eb19d
CaptStorm <cptstormbird@gmail.com>
2017-10-16 17:47:16 -0700

    Create staticMethodClass.js

---
+++ staticMethodClass.js
@@ -0,0 +1,30 @@
+class Calculator {
+  static multiply (a, b){
+    return a*b;
+  }
+}
+
+let a = Calculator.multiply(5,7);
+console.log(a);
+
+///////////////////// static
+
+
+class Calculator {
+  static multiply (a, b){
+    return a*b;
+  }
+  
+    static add(a, b){
+    return a+b;
+  }
+  
+}
+
+let a = Calculator.multiply(5,7);
+console.log(a);
+
+let b = Calculator.add(5,7);
+console.log(b);
+
+//////////////////////////////
